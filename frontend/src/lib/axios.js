55978f9a446273bba5e0fcf0725cac30d3e072cd
Tanish_Nagpal <tanish1739.becse24@chitkara.edu.in>
2026-03-08 00:09:10 +0530

    axios.js edits

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+
 console.log("API:", import.meta.env.VITE_API_URL);
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, //brwoser will send cookies with requests to the server automatically 
-  // on every single request 
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
