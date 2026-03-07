1e4622dd6102ff18e4eca0586a35072d7bbe4902
Tanish_Nagpal <tanish1739.becse24@chitkara.edu.in>
2026-03-08 00:30:13 +0530

    updates axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,20 +1,8 @@
 import axios from "axios";
-import { getAuth } from "@clerk/clerk-react";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-});
-
-axiosInstance.interceptors.request.use(async (config) => {
-  const { getToken } = getAuth();
-  const token = await getToken();
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
 });
 
 export default axiosInstance;
