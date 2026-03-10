f1d2bf7c63d7529aa03780664a8f5d994fe9d1c6
Tanish_Nagpal <tanish1739.becse24@chitkara.edu.in>
2026-03-10 11:25:16 +0530

    added stream.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import ProblemsPage from "./pages/ProblemsPage";
 import { Toaster } from "react-hot-toast";
 import DashboardPage from "./pages/DashboardPage";
 import ProblemPage from "./pages/ProblemPage";
+import SessionPage from "./pages/SessionPage";
 
 function App() {useUser();
   
@@ -22,6 +23,7 @@ function App() {useUser();
 
         <Route path = "/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
         <Route path = "/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+        <Route path = "/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
       </Routes>
       <Toaster toastOptions={{duration:3000}}/>
     </>
