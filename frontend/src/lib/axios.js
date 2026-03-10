402d55c83cec3fa7b72a7940608f590dea5920e3
Tanish_Nagpal <tanish1739.becse24@chitkara.edu.in>
2026-03-10 11:47:48 +0530

    changes in sessions and axios routes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,10 @@ import axios from "axios";
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  // This ensures there's always a slash between the base and the path
+  baseURL: import.meta.env.VITE_API_URL.endsWith('/') 
+    ? import.meta.env.VITE_API_URL 
+    : `${import.meta.env.VITE_API_URL}/`,
   withCredentials: true,
 });
 
