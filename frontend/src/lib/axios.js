accdf023160ebd10aa8f2b2a5413b6ec804d8986
Tanish_Nagpal <tanish1739.becse24@chitkara.edu.in>
2026-03-10 11:42:19 +0530

    minor changes in axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
